1cc51f56ae7e57bf26a9511fb91150c2e7592807
Slamjack <murlouk@gmail.com>
2016-09-30 20:08:36 +0200

    Created main.c and Makefile
    Maybe make a big include.h
    Place your .c in the makefile

---
+++ sources/main.c
@@ -0,0 +1,22 @@
+﻿/*
+|
+|	███╗   ███╗ █████╗ ██╗███╗   ██╗    ██████╗
+|	████╗ ████║██╔══██╗██║████╗  ██║   ██╔════╝
+|	██╔████╔██║███████║██║██╔██╗ ██║   ██║
+|	██║╚██╔╝██║██╔══██║██║██║╚██╗██║   ██║
+|	██║ ╚═╝ ██║██║  ██║██║██║ ╚████║██╗╚██████╗			
+|	╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚═╝ ╚═════╝	
+|
+*/
+
+//TODO : Maybe make a big includes.h folder ?
+#pragma region INCLUDES
+
+#include <stdio.h>
+
+#pragma endregion
+
+int main(int argc, const char* argv[])
+{
+	printf("\nHello World\n\n");
+}
